d4f427043e49974a1dd8c9336e3634699a4df7da
hispatv <hispacam@proton.me>
2025-10-28 23:35:25 +0100

    Fixed installer no plugin folder

--- installer.sh
+++ installer.sh
@@ -1,5 +1,6 @@
 echo "Installing HispaTV Plugin..."
 echo '#SERVICE 1:7:1:0:0:0:0:0:0:0:FROM BOUQUET "userbouquet.Hispa.tv" ORDER BY bouquet' >> /etc/enigma2/bouquets.tv
+mkdir /usr/lib/enigma2/python/Plugins/Extensions/HispaTV
 wget --no-check-certificate "https://github.com/hispatv/hispatv/raw/refs/heads/main/plugin.pyc" -O /usr/lib/enigma2/python/Plugins/Extensions/HispaTV/plugin.pyc
 wget --no-check-certificate "https://raw.githubusercontent.com/hispatv/hispatv/refs/heads/main/update.sh" -O /usr/lib/enigma2/python/Plugins/Extensions/HispaTV/update.sh
 wget --no-check-certificate "https://github.com/hispatv/hispatv/raw/refs/heads/main/plugin.png" -O /usr/lib/enigma2/python/Plugins/Extensions/HispaTV/plugin.png

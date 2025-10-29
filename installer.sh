52ece9bbf251ac7cc592e57ee8aa7da1f45bdafb
hispatv <hispacam@proton.me>
2025-10-29 01:16:54 +0100

    updated installer

--- installer.sh
+++ installer.sh
@@ -1,10 +1,25 @@
 echo "Installing HispaTV Plugin..."
-echo '#SERVICE 1:7:1:0:0:0:0:0:0:0:FROM BOUQUET "userbouquet.Hispa.tv" ORDER BY bouquet' >> /etc/enigma2/bouquets.tv
-mkdir /usr/lib/enigma2/python/Plugins/Extensions/HispaTV
+
+# Línea que queremos insertar
+LINE='#SERVICE 1:7:1:0:0:0:0:0:0:0:FROM BOUQUET "userbouquet.Hispa.tv" ORDER BY bouquet'
+FILE="/etc/enigma2/bouquets.tv"
+
+# Comprobar si la línea ya existe
+if ! grep -Fxq "$LINE" "$FILE"; then
+    echo "$LINE" >> "$FILE"
+fi
+
+# Crear directorio del plugin
+mkdir -p /usr/lib/enigma2/python/Plugins/Extensions/HispaTV
+
+# Descargar archivos
 wget --no-check-certificate "https://github.com/hispatv/hispatv/raw/refs/heads/main/plugin.pyc" -O /usr/lib/enigma2/python/Plugins/Extensions/HispaTV/plugin.pyc
 wget --no-check-certificate "https://raw.githubusercontent.com/hispatv/hispatv/refs/heads/main/update.sh" -O /usr/lib/enigma2/python/Plugins/Extensions/HispaTV/update.sh
 wget --no-check-certificate "https://github.com/hispatv/hispatv/raw/refs/heads/main/plugin.png" -O /usr/lib/enigma2/python/Plugins/Extensions/HispaTV/plugin.png
+
+# Cambiar permisos
 chmod 775 /usr/lib/enigma2/python/Plugins/Extensions/HispaTV/plugin.pyc
 chmod 775 /usr/lib/enigma2/python/Plugins/Extensions/HispaTV/update.sh
+
 echo "Installation completed. Rebooting the system..."
 reboot
